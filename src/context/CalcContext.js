a3583606654c1eab9939e454de78d42341b9cf09
Bakarijack <bakarikilu254@gmail.com>
2022-12-04 11:29:24 +0300

    calculator ready

---
+++ src/context/CalcContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from 'react'
+
+export const CalcContext = createContext()
+
+const CalProvider = ({children}) => {
+  const [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0
+  });
+
+const providerValue = {
+  calc, setCalc
+}
+  return (
+    <CalcContext.Provider value={providerValue}>
+      {children}
+    </CalcContext.Provider>
+  )
+}
+
+export default CalProvider
